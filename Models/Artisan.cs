e810647373f12abc01bbbfd0266eec0f21b8fb4f
Savindu-Thilakshana <savinduthilakshana12@gmail.com>
2026-07-07 12:57:37 +0530

    Created Artisan class and ArtisanListForm

---
+++ Models/Artisan.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+
+namespace ArtisanMarketAdmin.Models
+{
+    public class Artisan : User
+    {
+        public int ArtisanId { get; set; }
+
+        public Artisan(int artisanId, string name, string email, string contact, string status)
+            : base(name, email, contact, status)
+        {
+            ArtisanId = artisanId;
+        }
+    }
+}
