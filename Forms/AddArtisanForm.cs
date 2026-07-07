bd0e0944cc9e141ebaf58c901615ee1d38a720d8
Savindu-Thilakshana <savinduthilakshana12@gmail.com>
2026-07-07 21:06:37 +0530

    Created Add Artisan Form

---
+++ Forms/AddArtisanForm.cs
@@ -0,0 +1,51 @@
+﻿using ArtisanMarketAdmin.Data;
+using ArtisanMarketAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ArtisanMarketAdmin.Forms
+{
+    public partial class AddArtisanForm : Form
+    {
+        private readonly DatabaseHelper _db = new DatabaseHelper();
+
+        public AddArtisanForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                var applicant = new PendingArtisan(0, txtName.Text, txtEmail.Text, txtContact.Text);
+
+                _db.ExecuteNonQuery(
+                    "INSERT INTO Artisans (Name, Email, Contact, Status) VALUES (@n, @e, @c, @s)",
+                    new Dictionary<string, object>
+                    {
+                        { "@n", txtName.Text },
+                        { "@e", txtEmail.Text },
+                        { "@c", txtContact.Text },
+                        { "@s", applicant.ApprovalStatus } 
+                    });
+
+                MessageBox.Show("Artisan registered - now awaiting approval.");
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); 
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
