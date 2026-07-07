ec6797709cf59a38805ae22ecb95a5e750b1c5b4
NadithDulwan <nadithdulwan11@gmail.com>
2026-07-07 13:48:34 +0530

    Added  and Design ItemListForm in Forms

---
+++ Forms/ItemListForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ArtisanMarketAdmin.Forms
+{
+    partial class ItemListForm
+    {
+     
+        private System.ComponentModel.IContainer components = null;
+
+      
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+     
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            txtSearchName = new TextBox();
+            btnSearch = new Button();
+            dgvItems = new DataGridView();
+            btnAddNew = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            productBindingSource = new BindingSource(components);
+            ((System.ComponentModel.ISupportInitialize)dgvItems).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)productBindingSource).BeginInit();
+            SuspendLayout();
+           
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Coral;
+            label1.Location = new Point(64, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Search:";
+          
+            txtSearchName.Location = new Point(126, 46);
+            txtSearchName.Name = "txtSearchName";
+            txtSearchName.Size = new Size(165, 27);
+            txtSearchName.TabIndex = 1;
+           
+            btnSearch.BackColor = Color.Coral;
+            btnSearch.Location = new Point(329, 46);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(94, 29);
+            btnSearch.TabIndex = 3;
+            btnSearch.Text = " Search";
+            btnSearch.UseVisualStyleBackColor = false;
+            btnSearch.Click += btnSearch_Click;
+           
+            dgvItems.AllowUserToAddRows = false;
+            dgvItems.AllowUserToDeleteRows = false;
+            dgvItems.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvItems.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvItems.Location = new Point(12, 111);
+            dgvItems.MultiSelect = false;
+            dgvItems.Name = "dgvItems";
+            dgvItems.ReadOnly = true;
+            dgvItems.RowHeadersVisible = false;
+            dgvItems.RowHeadersWidth = 51;
+            dgvItems.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvItems.Size = new Size(758, 272);
+            dgvItems.TabIndex = 4;
+             
+            btnAddNew.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnAddNew.BackColor = Color.Olive;
+            btnAddNew.ForeColor = Color.White;
+            btnAddNew.Location = new Point(35, 401);
+            btnAddNew.Name = "btnAddNew";
+            btnAddNew.Size = new Size(155, 40);
+            btnAddNew.TabIndex = 5;
+            btnAddNew.Text = "Add New Item";
+            btnAddNew.UseVisualStyleBackColor = false;
+            btnAddNew.Click += btnAddNew_Click;
+             
+            btnEdit.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnEdit.BackColor = Color.Olive;
+            btnEdit.ForeColor = Color.White;
+            btnEdit.Location = new Point(196, 401);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(155, 40);
+            btnEdit.TabIndex = 6;
+            btnEdit.Text = "Edit Selected";
+            btnEdit.UseVisualStyleBackColor = false;
+            btnEdit.Click += btnEdit_Click;
+           
+            btnDelete.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnDelete.BackColor = Color.Olive;
+            btnDelete.ForeColor = Color.White;
+            btnDelete.Location = new Point(357, 401);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(155, 40);
+            btnDelete.TabIndex = 7;
+            btnDelete.Text = "Delete Selected";
+            btnDelete.UseVisualStyleBackColor = false;
+            btnDelete.Click += btnDelete_Click;
+           
+            productBindingSource.DataSource = typeof(Models.Product);
+            
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(251, 247, 241);
+            ClientSize = new Size(782, 453);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnAddNew);
+            Controls.Add(dgvItems);
+            Controls.Add(btnSearch);
+            Controls.Add(txtSearchName);
+            Controls.Add(label1);
+            Name = "ItemListForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Manage Items";
+            Load += ItemListForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvItems).EndInit();
+            ((System.ComponentModel.ISupportInitialize)productBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtSearchName;
+        private Button btnSearch;
+        private DataGridView dgvItems;
+        private Button btnAddNew;
+        private Button btnEdit;
+        private Button btnDelete;
+        private BindingSource productBindingSource;
+    }
+}
---
+++ Forms/ItemListForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using ArtisanMarketAdmin.Data;
+using ArtisanMarketAdmin.Models;
+
+namespace ArtisanMarketAdmin.Forms
+{
+    public partial class ItemListForm : Form
+    {
+        private readonly DatabaseHelper _db = new DatabaseHelper();
+
+        public ItemListForm()
+        {
+            InitializeComponent();
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+        }
+
+        private void ItemListForm_Load(object sender, EventArgs e)
+        {
+            LoadItems();
+        }
+
+        private void LoadItems()
+        {
+            dgvItems.DataSource = _db.ExecuteQueryToTable("SELECT * FROM Products");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string name = txtSearchName.Text;
+
+            dgvItems.DataSource = _db.ExecuteQueryToTable(
+            "SELECT * FROM Products WHERE Name LIKE @n",
+            new Dictionary<string, object> { { "@n", $"%{name}%" } });
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvItems.CurrentRow == null)
+            {
+                MessageBox.Show("Select a row first.");
+                return;
+            }
+
+            int id = Convert.ToInt32(dgvItems.CurrentRow.Cells["ProductId"].Value);
+
+            if (MessageBox.Show("Delete this item?", "Confirm", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Product.RemoveProduct(_db, id);
+                LoadItems();
+            }
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            new AddItemForm().ShowDialog();
+            LoadItems();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvItems.CurrentRow == null)
+            {
+                MessageBox.Show("Select a row first.");
+                return;
+            }
+
+            int id = Convert.ToInt32(dgvItems.CurrentRow.Cells["ProductId"].Value);
+            new EditItemForm(id).ShowDialog();
+            LoadItems();
+        }
+    }
+}
