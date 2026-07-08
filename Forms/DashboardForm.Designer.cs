c7af4f8c68ff053615c1f878de77bf7f6be51273
NadithDulwan <nadithdulwan11@gmail.com>
2026-07-08 11:46:39 +0530

    Added DashboardForm.cs to Forms.

--- Forms/DashboardForm.Designer.cs
+++ Forms/DashboardForm.Designer.cs
@@ -2,15 +2,9 @@
 {
     partial class DashboardForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+       
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +16,159 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+        
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DashboardForm";
+            lblTotalItems = new Label();
+            lblTotalArtisans = new Label();
+            lblTotalOrders = new Label();
+            btnManageItems = new Button();
+            btnArtisans = new Button();
+            btnPendingApplications = new Button();
+            btnSalesReport = new Button();
+            btnLogout = new Button();
+            btnRecordSale = new Button();
+            SuspendLayout();
+           
+            lblTotalItems.Anchor = AnchorStyles.Top;
+            lblTotalItems.AutoSize = true;
+            lblTotalItems.BackColor = Color.FromArgb(241, 228, 210);
+            lblTotalItems.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTotalItems.Location = new Point(67, 18);
+            lblTotalItems.Margin = new Padding(0);
+            lblTotalItems.Name = "lblTotalItems";
+            lblTotalItems.RightToLeft = RightToLeft.Yes;
+            lblTotalItems.Size = new Size(110, 34);
+            lblTotalItems.TabIndex = 0;
+            lblTotalItems.Text = "Items: 0";
+            lblTotalItems.Click += lblTotalItems_Click;
+            
+            lblTotalArtisans.Anchor = AnchorStyles.Top;
+            lblTotalArtisans.AutoSize = true;
+            lblTotalArtisans.BackColor = Color.FromArgb(243, 220, 210);
+            lblTotalArtisans.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold);
+            lblTotalArtisans.Location = new Point(235, 18);
+            lblTotalArtisans.Margin = new Padding(0);
+            lblTotalArtisans.Name = "lblTotalArtisans";
+            lblTotalArtisans.RightToLeft = RightToLeft.Yes;
+            lblTotalArtisans.Size = new Size(150, 34);
+            lblTotalArtisans.TabIndex = 1;
+            lblTotalArtisans.Text = "Artisans: 0 ";
+            lblTotalArtisans.Click += lblTotalArtisans_Click;
+           
+            lblTotalOrders.Anchor = AnchorStyles.Top;
+            lblTotalOrders.AutoSize = true;
+            lblTotalOrders.BackColor = Color.FromArgb(220, 234, 224);
+            lblTotalOrders.Font = new Font("Bodoni MT", 16.2F, FontStyle.Bold);
+            lblTotalOrders.Location = new Point(452, 18);
+            lblTotalOrders.Margin = new Padding(0);
+            lblTotalOrders.Name = "lblTotalOrders";
+            lblTotalOrders.RightToLeft = RightToLeft.Yes;
+            lblTotalOrders.Size = new Size(121, 34);
+            lblTotalOrders.TabIndex = 2;
+            lblTotalOrders.Text = "Orders: 0";
+            lblTotalOrders.Click += lblTotalOrders_Click;
+          
+            btnManageItems.AutoSize = true;
+            btnManageItems.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnManageItems.Location = new Point(19, 84);
+            btnManageItems.Margin = new Padding(10);
+            btnManageItems.Name = "btnManageItems";
+            btnManageItems.Size = new Size(222, 38);
+            btnManageItems.TabIndex = 3;
+            btnManageItems.Text = "Manage Items";
+            btnManageItems.UseVisualStyleBackColor = true;
+            btnManageItems.Click += btnManageItems_Click;
+            
+            btnArtisans.AutoSize = true;
+            btnArtisans.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnArtisans.Location = new Point(19, 152);
+            btnArtisans.Margin = new Padding(10);
+            btnArtisans.Name = "btnArtisans";
+            btnArtisans.Size = new Size(222, 38);
+            btnArtisans.TabIndex = 4;
+            btnArtisans.Text = "View Artisans";
+            btnArtisans.UseVisualStyleBackColor = true;
+            btnArtisans.Click += btnArtisans_Click;
+           
+            btnPendingApplications.AutoSize = true;
+            btnPendingApplications.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnPendingApplications.Location = new Point(19, 219);
+            btnPendingApplications.Margin = new Padding(10);
+            btnPendingApplications.Name = "btnPendingApplications";
+            btnPendingApplications.Size = new Size(222, 38);
+            btnPendingApplications.TabIndex = 5;
+            btnPendingApplications.Text = "Pending Applications";
+            btnPendingApplications.UseVisualStyleBackColor = true;
+            btnPendingApplications.Click += btnPendingApplications_Click;
+           
+            btnSalesReport.AutoSize = true;
+            btnSalesReport.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSalesReport.Location = new Point(19, 353);
+            btnSalesReport.Margin = new Padding(10);
+            btnSalesReport.Name = "btnSalesReport";
+            btnSalesReport.Size = new Size(222, 38);
+            btnSalesReport.TabIndex = 6;
+            btnSalesReport.Text = "Sales Report";
+            btnSalesReport.UseVisualStyleBackColor = true;
+            btnSalesReport.Click += btnSalesReport_Click;
+           
+            btnLogout.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnLogout.AutoSize = true;
+            btnLogout.BackColor = Color.FromArgb(220, 234, 224);
+            btnLogout.Cursor = Cursors.Hand;
+            btnLogout.ForeColor = Color.FromArgb(37, 68, 53);
+            btnLogout.Location = new Point(557, 345);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(124, 55);
+            btnLogout.TabIndex = 7;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = false;
+            btnLogout.Click += btnLogout_Click;
+           
+            btnRecordSale.AutoSize = true;
+            btnRecordSale.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRecordSale.Location = new Point(19, 286);
+            btnRecordSale.Margin = new Padding(10);
+            btnRecordSale.Name = "btnRecordSale";
+            btnRecordSale.Size = new Size(222, 38);
+            btnRecordSale.TabIndex = 8;
+            btnRecordSale.Text = "Record Sale";
+            btnRecordSale.UseVisualStyleBackColor = true;
+            btnRecordSale.Click += btnRecordSale_Click;
+           
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackgroundImage = Properties.Resources.dashboard_background;
+            ClientSize = new Size(682, 403);
+            Controls.Add(btnLogout);
+            Controls.Add(btnManageItems);
+            Controls.Add(btnSalesReport);
+            Controls.Add(btnRecordSale);
+            Controls.Add(btnPendingApplications);
+            Controls.Add(btnArtisans);
+            Controls.Add(lblTotalOrders);
+            Controls.Add(lblTotalItems);
+            Controls.Add(lblTotalArtisans);
+            Name = "DashboardForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Dashboard - Artisan Market Admin";
+            Load += DashboardForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblTotalItems;
+        private Label lblTotalArtisans;
+        private Label lblTotalOrders;
+        private Button btnManageItems;
+        private Button btnArtisans;
+        private Button btnPendingApplications;
+        private Button btnSalesReport;
+        private Button btnLogout;
+        private Button btnRecordSale;
     }
 }
--- Forms/DashboardForm.cs
+++ Forms/DashboardForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArtisanMarketAdmin.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,5 +15,65 @@ namespace ArtisanMarketAdmin.Forms
         {
             InitializeComponent();
         }
+
+        private void btnManageItems_Click(object sender, EventArgs e)
+        {
+            new ItemListForm().ShowDialog();
+        }
+
+        private void btnArtisans_Click(object sender, EventArgs e)
+        {
+            new ArtisanListForm().ShowDialog();
+        }
+
+        private void btnPendingApplications_Click(object sender, EventArgs e)
+        {
+            new PendingApplicationsForm().ShowDialog();
+        }
+
+        private void btnSalesReport_Click(object sender, EventArgs e)
+        {
+            new SalesReportForm().ShowDialog();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnRecordSale_Click(object sender, EventArgs e)
+        {
+            new RecordSaleForm().ShowDialog();
+        }
+
+
+        private DatabaseHelper _db = new DatabaseHelper();
+
+        private void DashboardForm_Load(object sender, EventArgs e)
+        {
+            lblTotalItems.Text = "Items: " + _db.ExecuteScalar("SELECT COUNT(*) FROM Products");
+            lblTotalArtisans.Text = "Artisans: " + _db.ExecuteScalar("SELECT COUNT(*) FROM Artisans WHERE Status = 'Active'");
+            lblTotalOrders.Text = "Orders: " + _db.ExecuteScalar("SELECT COUNT(*) FROM Orders");
+        }
+
+        private void lblTotalItems_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTotalArtisans_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTotalOrders_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
+
 }
